7db5714aeaa769d60a7b9d18d641f55a8322a29b
Stephen Nielsen <smnielsen256@byui.edu>
2022-06-27 17:39:00 -0600

    Answered question 2

--- README.md
+++ README.md
@@ -7,8 +7,9 @@ This program analyses a dataset showing the the Sustainabl Development Report (S
 # Data Analysis Results
 
 ## Question 1: What country has shown the most improvement over time?
-*   Indonesia improved the most, with a difference of 9.530000000000008. It changed from 59.37 in 2000, to 68.9 in 2021.
-## Question 2: What was the largest improvement between two consecutive years?
+* Indonesia improved the most, with a difference of 9.530000000000008. It changed from 59.37 in 2000, to 68.9 in 2021.
+## Question 2: What was the largest improvement between two consecutive years in the US?
+* The largest difference in the US between two consecutive years was 2.349, between 2013 and 2014.
 
 # Development Environment
 
@@ -21,5 +22,8 @@ This program analyses a dataset showing the the Sustainabl Development Report (S
 * [Adding a new column to a DataFrame](https://pythonguides.com/add-a-column-to-a-dataframe-in-python-pandas/)
 * [pandas: sort](https://www.geeksforgeeks.org/how-to-sort-pandas-dataframe/)
 * [pandas: iloc](https://www.geeksforgeeks.org/how-to-get-first-row-of-pandas-dataframe/)
+* [pandas: column iteration](https://www.geeksforgeeks.org/loop-or-iterate-over-all-or-certain-columns-of-a-dataframe-in-python-pandas/)
+* [pandas: filter and query](https://pythonspot.com/pandas-filter/)
+* [Finding the index of the largest number in an array](https://stackoverflow.com/questions/27755867/returning-the-index-of-the-largest-element-in-an-array-in-python)
 
 # Future Work
--- data_analysis.py
+++ data_analysis.py
@@ -39,4 +39,72 @@ print(f"{print_country} improved the most, with a difference of {print_SDR_diffe
     It changed from {print_start} in 2000, to {print_end} in 2021.")
 
 # Question 2:
-# What was the largest improvement between two consecutive years?
+# What was the largest improvement between two consecutive years in the U.S?
+
+# keep only columns needed for calculation
+data_2 = pd.DataFrame({
+    "Country" : data.Country,
+    "y_2000" : data.y_2000,
+    "y_2001" : data.y_2001,
+    "y_2002" : data.y_2002,
+    "y_2003" : data.y_2003,
+    "y_2004" : data.y_2004,
+    "y_2005" : data.y_2005,
+    "y_2006" : data.y_2006,
+    "y_2007" : data.y_2007,
+    "y_2008" : data.y_2008,
+    "y_2009" : data.y_2009,
+    "y_2010" : data.y_2010,
+    "y_2011" : data.y_2011,
+    "y_2012" : data.y_2012,
+    "y_2013" : data.y_2013,
+    "y_2014" : data.y_2014,
+    "y_2015" : data.y_2015,
+    "y_2016" : data.y_2016,
+    "y_2017" : data.y_2017,
+    "y_2018" : data.y_2018,
+    "y_2019" : data.y_2019,
+    "y_2020" : data.y_2020,
+    "y_2021" : data.y_2021
+    })
+
+# select data for target country
+target_country_values = data_2.query("Country == 'United States'")
+
+
+
+# array holding the differences of values between years.
+# diff[0] holds the difference between 2000 and 2001, diff[15] for the difference between 2015 and 2016, etc.
+diff = [
+    float(target_country_values["y_2001"] - target_country_values["y_2000"]),
+    float(target_country_values["y_2002"] - target_country_values["y_2001"]),
+    float(target_country_values["y_2003"] - target_country_values["y_2002"]),
+    float(target_country_values["y_2004"] - target_country_values["y_2003"]),
+    float(target_country_values["y_2005"] - target_country_values["y_2004"]),
+    float(target_country_values["y_2006"] - target_country_values["y_2005"]),
+    float(target_country_values["y_2007"] - target_country_values["y_2006"]),
+    float(target_country_values["y_2008"] - target_country_values["y_2007"]),
+    float(target_country_values["y_2009"] - target_country_values["y_2008"]),
+    float(target_country_values["y_2010"] - target_country_values["y_2009"]),
+    float(target_country_values["y_2011"] - target_country_values["y_2010"]),
+    float(target_country_values["y_2012"] - target_country_values["y_2011"]),
+    float(target_country_values["y_2002"] - target_country_values["y_2012"]),
+    float(target_country_values["y_2013"] - target_country_values["y_2001"]),
+    float(target_country_values["y_2014"] - target_country_values["y_2013"]),
+    float(target_country_values["y_2015"] - target_country_values["y_2014"]),
+    float(target_country_values["y_2016"] - target_country_values["y_2015"]),
+    float(target_country_values["y_2017"] - target_country_values["y_2016"]),
+    float(target_country_values["y_2018"] - target_country_values["y_2017"]),
+    float(target_country_values["y_2019"] - target_country_values["y_2018"]),
+    float(target_country_values["y_2020"] - target_country_values["y_2019"]),
+    float(target_country_values["y_2021"] - target_country_values["y_2020"])
+]
+
+# find largest difference
+largest_diff = max(diff)
+largest_diff_index = diff.index(max(diff))
+
+print(f"The largest difference in the US between two consecutive years: {largest_diff}, between 20{largest_diff_index} and 20{(largest_diff_index + 1)}.")
+
+
+
